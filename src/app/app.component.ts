7fb9c05112b359aa697cad84d1406af6191ce7c2
fabio campos <eaglebooks77@gmail.com>
2021-10-16 16:45:09 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Exercise } from './exercise';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  config = false;
+  exercises: Exercise[] = [{
+    name: 'Abdominal',
+    duration: 30,
+    repetitions: 3,
+    preparation: 15,
+    rest: 20
+  }];
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ConfigComponent } from './config/config.component';
+import { TimerComponent } from './timer/timer.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConfigComponent,
+    TimerComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
